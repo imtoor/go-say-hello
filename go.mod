20c086fc18a89330d481ecd1e6bcc8e06b748a4b
imtoor <reynaldyrahmat@gmail.com>
2023-06-25 13:26:51 +0800

    ref: add parameter age and sex

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/imtoor/go-say-hello
+module github.com/imtoor/go-say-hello/v2
 
 go 1.20
--- say_hello.go
+++ say_hello.go
@@ -1,9 +1,21 @@
 package gosayhello
 
-func SayHello(name string) string {
+func SayHello(name string, age int, sex string) string {
 	if name == "" {
 		return "Hi"
 	} else {
-		return "Hello " + name + ",\nHave a nice day!"
+		var greeting string
+		switch age >= 25 {
+		case true:
+			if sex == "male" {
+				greeting = "Mr."
+			} else if sex == "female" {
+				greeting = "Ms."
+			}
+		case false:
+			greeting = ""
+		}
+
+		return "Hello " + greeting + name + ",\nHave a nice day!"
 	}
 }
